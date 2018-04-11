e1e5979719a57b560b27607a1cb803de262db502
Hiroya NAKAJIMA <hiroya.nakajima@gmail.com>
2018-04-11 16:37:29 +0900

    完成

--- Music/ViewController.swift
+++ Music/ViewController.swift
@@ -7,9 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioplayer: AVAudioPlayer!
+    
+    
+    @IBAction func natsuPlayButton(){
+        setAudioPlayer(soundName: "natsu", type: "mp3")
+        audioplayer.play()
+    }
+    @IBAction func amePlayButton(){
+        setAudioPlayer(soundName: "ame", type: "mp3")
+        audioplayer.play()
+    }
+    @IBAction func akiPlayButton(){
+        setAudioPlayer(soundName: "aki", type: "mp3")
+        audioplayer.play()
+    }
+    
+    func setAudioPlayer(soundName:String,type:String){
+        let soundFilePath = Bundle.main.path(forResource: soundName, ofType: type)!
+        let fileURL = URL(fileURLWithPath: soundFilePath)
+        
+        do {
+            audioplayer = try AVAudioPlayer(contentsOf: fileURL)
+        }catch{
+            print("ファイルが読み込みませんでした")
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
